406036f93b1c8ecd6ef3647dd875859ba18a5818
Mujju-palaan <mdmujahid97@gmail.com>
2025-02-01 12:15:53 +0530

    store Chapter 7(a)

--- Store_management_system/Mujju-pg-Store-C07(a)-DQL.sql
+++ Store_management_system/Mujju-pg-Store-C07(a)-DQL.sql
@@ -659,4 +659,3 @@ select
 
 
 --GOOD LUCK WITH YOUR ASSIGNMENT!!!
---Don't forget to contact us if you need any further assistance with your assignments, and most importantly, for a manual review and approval of your work.
